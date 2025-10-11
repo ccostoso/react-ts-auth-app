101bfe90b3c20b7d5dd439aa43275aaa6bda6691
Carlos Costoso <carlos.costoso@outlook.com>
2025-10-11 18:55:59 -0400

    Sets routes for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import './App.css';
-import { Button } from './components/ui/button';
-import { Input } from './components/ui/input';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Homepage from './pages/home';
+import LoginPage from './pages/login';
+import ForgotPasswordPage from './pages/forgot-password';
+import ResetPasswordPage from './pages/reset-password';
+import VerifyEmailPage from './pages/verify-email';
+import RegisterPage from './pages/register';
 
 function App() {
   return (
-    <div className="p-10 flex flex-col gap-5">
-      <h1 className="text-xl font-bold">Homepage</h1>
-      <h1 className="text-sm font-bold">App Component</h1>
-      <Button variant="default">Shadcn default button</Button>
-      <Input placeholder="Enter name" />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* Private Route */}
+        <Route path="/" Component={Homepage} />
+
+        {/* Public Routes */}
+        <Route path="/register" Component={RegisterPage} />
+        <Route path="/login" Component={LoginPage} />
+        <Route path="/forgot-password" Component={ForgotPasswordPage} />
+        <Route path="/reset-password" Component={ResetPasswordPage} />
+        <Route path="/verify-email" Component={VerifyEmailPage} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
