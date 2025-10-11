a81bf1a321eff8a15321b0b63eab443659fdbd97
Carlos Costoso <carlos.costoso@outlook.com>
2025-10-11 17:50:14 -0400

    Adds shadcn button and input components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css';
+import { Button } from './components/ui/button';
+import { Input } from './components/ui/input';
 
 function App() {
   return (
     <div className="p-10 flex flex-col gap-5">
       <h1 className="text-xl font-bold">Homepage</h1>
       <h1 className="text-sm font-bold">App Component</h1>
+      <Button variant="default">Shadcn default button</Button>
+      <Input placeholder="Enter name" />
     </div>
   );
 }
