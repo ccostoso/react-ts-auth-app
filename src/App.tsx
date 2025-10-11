7705dafe3fef250243a9832dc5e8b0ce34fee40f
Carlos Costoso <carlos.costoso@outlook.com>
2025-10-11 16:38:36 -0400

    initial commits, adds tailwind and shadcn

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="p-10 flex flex-col gap-5">
+      <h1 className="text-xl font-bold">Homepage</h1>
+      <h1 className="text-sm font-bold">App Component</h1>
+    </div>
+  );
+}
+
+export default App;
